649acf48271f41eec98e17b2ca2dd90e739092d1
Yemi Djayeola <yemid2k18@gmail.com>
2023-10-14 08:20:52 +0000

    docker installation on centos 7

---
+++ docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo yum update
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker --version
+docker run hello-world
+
